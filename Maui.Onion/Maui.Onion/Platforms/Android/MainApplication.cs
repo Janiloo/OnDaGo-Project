5106f27fc5ea5490aebd2c1290ea416f654db020
Janiloo <jnavancena1@gmail.com>
2024-10-21 02:11:21 +0800

    deploy

---
+++ Maui.Onion/Maui.Onion/Platforms/Android/MainApplication.cs
@@ -0,0 +1,18 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace Maui.Onion
+{
+    [Application]
+    [MetaData("com.google.android.maps.v2.API_KEY",
+        Value = "AIzaSyBNWpBKVmqLmE3-nP9sKuQHTN8Jn40pN4w")]
+    public class MainApplication : MauiApplication
+    {
+        public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+            : base(handle, ownership)
+        {
+        }
+
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
